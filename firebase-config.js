268c35f79ab4c7713d633552ef102d9ce323b763
David <119540463+DavoMTZ@users.noreply.github.com>
2026-07-17 14:10:07 -0600

    fix: Subir firebase-config para que funcione en GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgg2rmLrCwTb7_eldzCJkomY2IcbOw25E",
+    authDomain: "weddd-75ad0.firebaseapp.com",
+    projectId: "weddd-75ad0",
+    storageBucket: "weddd-75ad0.firebasestorage.app",
+    messagingSenderId: "671357292421",
+    appId: "1:671357292421:web:41e0e26526dfb646a87eec",
+    measurementId: "G-E9PMB2M0DF"
+};
